b77fac10e9f553feb4e1249b98a860055e98b417
hindu <hindumathiavuluri@gmail.com>
2024-09-08 08:10:54 +0000

    Solution

--- src/components/FailureView/index.js
+++ src/components/FailureView/index.js
@@ -1,5 +1,5 @@
 import {
-  FailedView,
+  FailedViewDiv,
   FailedImage,
   FailedHeading,
   FailedNote,
@@ -27,7 +27,7 @@ const FailureView = props => {
           : 'https://assets.ccbp.in/frontend/react-js/nxt-watch-failure-view-light-theme-img.png'
 
         return (
-          <FailedView>
+          <FailedViewDiv>
             <FailedImage src={failureImageUrl} alt="failure view" />
             <FailedHeading headingColor={headingColor}>
               Oops! Something Went Wrong
@@ -39,7 +39,7 @@ const FailureView = props => {
             <RetryButton type="button" onClick={onClickRetry}>
               Retry
             </RetryButton>
-          </FailedView>
+          </FailedViewDiv>
         )
       }}
     </ThemeAndVideoContext.Consumer>
